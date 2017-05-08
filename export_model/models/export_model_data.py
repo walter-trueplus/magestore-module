2c1fbbdb51492e9122331cbca0dab335b36fe294
walter90 <walter@trueplus.vn>
2017-05-09 05:51:49 +0700

    walter - exporting model name

--- export_model/models/export_model_data.py
+++ export_model/models/export_model_data.py
@@ -7,22 +7,21 @@ class ExportModelData(models.Model):
     _name = "checking_model"
 
     ir_model = fields.Many2one('ir.model')
-    model_fields = fields.Many2many('ir.model.fields', compute='_compute_field')
+    # model_fields = fields.Many2many('ir.model.fields', compute='_compute_field')
 
-    @api.depends('ir_model')
-    def _compute_field(self):
-        for obj in self:
-            obj.model_fields = self.env['ir.model'].search([('model', '=', self.ir_model.model)])
+    # @api.depends('ir_model')
+    # def _compute_field(self):
+    #     for obj in self:
+    #         obj.model_fields = self.env['ir.model'].search([('model', '=', self.ir_model.model)])
 
     def export_data(self):
         print "enter here"
         print "----------"
-        res = self.env['ir.model'].search([('model', '=', 'product.product')], limit=1)
+        res = self.env['ir.model'].search([('model', '=', self.ir_model.model)], limit=1)
         if res:
             for item in res.field_id:
-                # print item.name
                 if item.name == "name":
-                    product_ids = self.env['product.product'].search([])
+                    product_ids = self.env[self.ir_model.model].search([])
                     for product_item in product_ids:
                         print product_item.name
 

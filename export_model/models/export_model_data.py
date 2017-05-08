95d909bc55b3e767d500a30837b5d0cceec58369
walter90 <walter@trueplus.vn>
2017-05-09 05:44:05 +0700

    walter - preparing base

--- export_model/models/export_model_data.py
+++ export_model/models/export_model_data.py
@@ -7,11 +7,22 @@ class ExportModelData(models.Model):
     _name = "checking_model"
 
     ir_model = fields.Many2one('ir.model')
+    model_fields = fields.Many2many('ir.model.fields', compute='_compute_field')
 
-    @api.multi
+    @api.depends('ir_model')
+    def _compute_field(self):
+        for obj in self:
+            obj.model_fields = self.env['ir.model'].search([('model', '=', self.ir_model.model)])
+
     def export_data(self):
         print "enter here"
         print "----------"
-        res = self.env['ir.model'].search([])
-        for item in res:
-            print item.name
+        res = self.env['ir.model'].search([('model', '=', 'product.product')], limit=1)
+        if res:
+            for item in res.field_id:
+                # print item.name
+                if item.name == "name":
+                    product_ids = self.env['product.product'].search([])
+                    for product_item in product_ids:
+                        print product_item.name
+

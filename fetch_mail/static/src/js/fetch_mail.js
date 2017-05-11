a016dcf791face94e8a4167e078d7ad98341e8a1
XMars <xtruongptit@gmail.com>
2017-05-11 15:42:50 +0700

    mars - combine two menu of fetch mail module to one + add button Fetchmail to list view of fetched mail

--- fetch_mail/static/src/js/fetch_mail.js
+++ fetch_mail/static/src/js/fetch_mail.js
@@ -4,13 +4,14 @@ odoo.define('fetch.mail.run', function(require) {
     var core = require('web.core');
     var Model = require('web.Model');
     var Widget = require('web.Widget');
+    var ListView = require('web.ListView');
     var QWeb = core.qweb;
 
 
     var Fet = Widget.extend({
         // events is an object (mapping) of an event to the function
         // or method to call when the event is triggered
-//        className: '.p_test',
+        // className: '.p_test',
         template: 'Fetch',
         events: {
             'click .p_test': 'b_click',
@@ -21,8 +22,26 @@ odoo.define('fetch.mail.run', function(require) {
             fetch_model.call('run_fetch_mail',[[]]);
         },
 
-
     });
+
+    var Extends = ListView.include({
+        // add listener for fetch_mail button
+        render_buttons: function() {
+            this._super.apply(this, arguments);
+            this.$buttons.on('click', '.o_list_button_fetch_mail', this.proxy('fetch_all_mail'));
+
+        },
+        fetch_all_mail:function(){
+            var fetch_model = new Model('cron.fetch');
+            fetch_model.call('run_fetch_mail',[[]]);
+            this.do_action({
+                type: 'ir.actions.act_window',
+                res_model: 'mail.fetched',
+                views: [[false, 'tree'],[false, 'form']],
+            });
+        },
+    });
+
 
     core.action_registry.add('run.fetch.mail', Fet);
 });

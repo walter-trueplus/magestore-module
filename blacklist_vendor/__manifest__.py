cda8197bedfeebc29dca3b8fb0da33e87f13159e
walter <walter@trueplus.vn>
2017-05-15 12:10:38 +0700

    walter - fixing depends character from __manifest.py__ file

--- blacklist_vendor/__manifest__.py
+++ blacklist_vendor/__manifest__.py
@@ -1,10 +1,11 @@
 {
     'name': 'black list vendor',
-    'depend': ['base','sale'],
+    'depends': ['base','sale'],
     'desciption': 'module for helping manager vendors in black list',
     'data': [
         'view/vendor_view.xml',
         'view/setting.xml'
-    ]
+    ],
+    'application': True,
 
 }

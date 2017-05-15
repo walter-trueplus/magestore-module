12ed7201bff1e2fe3b7ddc2c91e7fc8c10086cf8
kriz <kriz@trueplus.vn>
2017-05-15 08:14:14 +0700

    upload export from any module

--- export_contact_vcard/wizard/export_contact_vcard.py
+++ export_contact_vcard/wizard/export_contact_vcard.py
@@ -7,11 +7,39 @@ import csv
 from odoo import api, fields, models
 
 
-class ExportContact(models.Model):
+class ExportContact(models.TransientModel):
     _name = 'export.contact.wizard'
 
     name = fields.Char(string="File Name", readonly=True)
-    data = fields.Binary(string="File", readonly=True, compute='download')
+    data = fields.Binary(string="File", readonly=True)
+
+    @api.multi
+    def do_export2(self, id):
+        if id:
+            with contextlib.closing(cStringIO.StringIO()) as buf:
+                writer = csv.writer(buf, delimiter=":", quotechar='"')
+                for item in self.env['res.partner'].search([('id', '=', id)], limit=1):
+                    print item
+                    writer.writerow(("BEGIN", "VCARD"))
+                    writer.writerow(("VERSION", "3.0"))
+                    writer.writerow(("END", "VCARD"))
+
+                out = base64.encodestring(buf.getvalue())
+            self.write({
+                'state': 'get',
+                'data': out,
+                'name': 'contact_info.vcf'
+            })
+        compose_form = self.env.ref('export_contact_vcard.wizard_export_contact')
+        # return {
+        #     'type': 'ir.actions.act_window',
+        #     'res_model': 'export.contact.wizard',
+        #     'view_mode': 'form',
+        #     'view_type': 'form',
+        #     'views': [(compose_form.id, 'form')],
+        #     'view_id': compose_form.id,
+        #     'target': 'new',
+        # }
 
 
 class DownloadContact(models.Model):
@@ -19,7 +47,10 @@ class DownloadContact(models.Model):
 
     @api.multi
     def download(self):
-        print self.name
+        # print self.name
+        # other_object = self.env['export.contact.wizard']
+        # other_object.do_export2(self.id)
+        # print self.id
         compose_form = self.env.ref('export_contact_vcard.wizard_export_contact')
         return {
             'type': 'ir.actions.act_window',

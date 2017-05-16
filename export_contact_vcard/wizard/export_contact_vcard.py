dbcb0a4493e737acd392189778d3a99a2e92da7f
kriz <kriz@trueplus.vn>
2017-05-16 11:25:37 +0700

    upload export from any module

--- export_contact_vcard/wizard/export_contact_vcard.py
+++ export_contact_vcard/wizard/export_contact_vcard.py
@@ -7,57 +7,6 @@ import csv
 from odoo import api, fields, models
 
 
-# class ExportContact(models.TransientModel):
-#     _name = 'export.contact.wizard'
-#
-#     name = fields.Char(string="File Name", readonly=True)
-#     data = fields.Binary(string="File", readonly=True)
-#
-#     @api.multi
-#     def do_export2(self, id):
-#         if id:
-#             print self.id
-#             print id
-#             with contextlib.closing(cStringIO.StringIO()) as buf:
-#                 writer = csv.writer(buf, delimiter=":", quotechar='"')
-#                 for item in self.env['res.partner'].search([('id', '=', id)], limit=1):
-#                     print item
-#                     writer.writerow(("BEGIN", "VCARD"))
-#                     writer.writerow(("VERSION", "3.0"))
-#                     writer.writerow(("END", "VCARD"))
-#
-#                 out = base64.encodestring(buf.getvalue())
-#             self.create({
-#                 'state': 'get',
-#                 'data': out,
-#                 'name': 'contact_info.vcf'
-#             })
-#         # compose_form = self.env.ref('export_contact_vcard.wizard_export_contact')
-#         # res = {
-#         #     'type': 'ir.actions.act_window',
-#         #     # 'name': _('Customer Invoice'),
-#         #     'res_model': 'export.contact.wizard',
-#         #     'view_type': 'form',
-#         #     'view_mode': 'form',
-#         #     'res_id': self.id,
-#         #     'views': [(compose_form.id, 'form')],
-#         #     'view_id': compose_form.id,
-#         #     'target': 'current',
-#         #     # 'context': {'default_partner_id': client_id}
-#         # }
-#
-#         compose_form = self.env.ref('export_contact_vcard.wizard_export_contact')
-#         return {
-#             'type': 'ir.actions.act_window',
-#             'res_model': 'export.contact.wizard',
-#             'view_mode': 'form',
-#             'view_type': 'form',
-#             'views': [(compose_form.id, 'form')],
-#             'view_id': compose_form.id,
-#             'target': 'new',
-#         }
-
-
 class DownloadContact(models.Model):
     # _name = 'export.contact.wizard'
     _inherit = 'res.partner'

cf49c4ab18e427b0a91ae5771e22d018f3c0640c
foxbattrueplus <foxbat@trueplus.vn>
2017-05-16 10:37:14 +0700

    foxbat- module blacklist_vendor

--- blacklist_vendor/models/Setting.py
+++ blacklist_vendor/models/Setting.py
@@ -5,10 +5,16 @@ from odoo import fields, models, api
 class Setting(models.TransientModel):
     _inherit = 'purchase.config.settings'
 
-    warning_option = fields.Selection([(0, 'No warning'),
-                                       (1, 'Warning'),
-                                       (2, 'Eject create purchase')]
-                                      ,
+    warning_option = fields.Selection([('no_warning', 'No warning'),
+                                       ('warning', 'Warning'),
+                                       ('eject', 'Eject create purchase')],
+                                      required=True,
+                                      default='warning',
                                       string="Notify:")
 
+
+    @api.multi
+    def set_warning_option_defaults(self):
+        return self.env['ir.values'].sudo().set_default(
+            'purchase.config.settings', 'warning_option', self.warning_option)
 

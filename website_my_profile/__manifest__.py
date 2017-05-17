b29339b73e1d26a96a94f9def9d8a023a67f096a
XMars <xtruongptit@gmail.com>
2017-05-17 16:24:33 +0700

    mars - change manifest file of website_my_profile module

--- website_my_profile/__manifest__.py
+++ website_my_profile/__manifest__.py
@@ -8,20 +8,18 @@
     'description': """
         This module will adds some property for /my/account page and /web/signup page:
         
-        + Add change password button to /my/account page: if that is not first time the user change password,Odoo will direct redirect to /web/reset_password page and the user can change the password directly
-        + Add and delete properties of /my/account page
+        + Add Change Password button to /my/account page: allow user change password directly,
+        don't redirect to /web/reset_password
+        
+        + Add Change Login button to /my/account page: allow user change login name (must input
+        current password)
+        
+        + Add and delete properties of /my/account page:
         
             + Delete Company Name and Zip / Postal Code properties
-            + Add Gender, Active and Birthday properties
+            + Add Login, Gender, Active and Birthday properties
 
-        If you choose reset password from /web/login url, an email will be send to
-        your email if your email address is valid, if not: the email will be send to
-        mars@trueplus.vn (can change)
-        
-        If you click Reset Password button in /my/account and it's your first time 
-        you reset password, an email will be send to
-        your email. 
-        If not first time, you will be redirect to /web/reset_password?reset_directly=directly""",
+        + Don't allow access to login, signup and reset_password page if user current loged in""",
 
     'author': "magestore.com",
     'website': "magestore.com",

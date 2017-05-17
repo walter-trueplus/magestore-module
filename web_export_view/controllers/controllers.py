c2bb63f90cae666c77222e62bd40422fd2d51155
Kriz <krizpham>
2017-05-17 16:58:57 +0700

    up load new module

--- web_export_view/controllers/controllers.py
+++ web_export_view/controllers/controllers.py
@@ -1,17 +1,12 @@
- # -*- coding: utf-8 -*-
-# Copyright 2016 Henry Zhou (http://www.maxodoo.com)
-# Copyright 2016 Rodney (http://clearcorp.cr/)
-# Copyright 2012 Agile Business Group
-# Copyright 2012 Therp BV
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# -*- coding: utf-8 -*-
 
 import json
 import odoo.http as http
 from odoo.http import request
-from odoo.addons.web.controllers.main import ExcelExport
+from odoo.addons.web.controllers.main import CSVExport
 
 
-class ExcelExportView(ExcelExport):
+class ExcelExportView(CSVExport):
     def __getattribute__(self, name):
         if name == 'fmt':
             raise AttributeError()
@@ -25,7 +20,7 @@ class ExcelExportView(ExcelExport):
         rows = data.get('rows', [])
 
         return request.make_response(
-            self.from_data_exp(columns_headers, rows),
+            self.from_data(columns_headers, rows),
             headers=[
                 ('Content-Disposition', 'attachment; filename="%s"'
                  % self.filename(model)),
@@ -33,7 +28,8 @@ class ExcelExportView(ExcelExport):
             ],
             cookies={'fileToken': token}
         )
-# class VCARDExport(ExportFormat, http.Controller):
+
+# class CSVExport(ExportFormat, http.Controller):
 #
 #     @http.route('/web/export/csv', type='http', auth="user")
 #     @serialize_exception
@@ -45,13 +41,27 @@ class ExcelExportView(ExcelExport):
 #         return 'text/csv;charset=utf8'
 #
 #     def filename(self, base):
-#         return base + '.csv'
+#         return base + '.vcf'
 #
-#     def from_data_exp(self, fields, rows):
+#     def from_data(self, fields, rows):
 #         fp = StringIO()
-#         writer = csv.writer(fp, quoting=csv.QUOTE_ALL)
-#
-#         writer.writerow([name.encode('utf-8') for name in fields])
+#         writer = csv.writer(fp, delimiter=":", quotechar='"')
+#         for data in rows:
+#             lst_data2 = []
+#             lst_data2.append(data)
+#             for lst_data in lst_data2:
+#                 writer.writerow(("BEGIN", "VCARD"))
+#                 writer.writerow(("VERSION", "3.0"))
+#                 writer.writerow(("N",lst_data[0] if lst_data else ''))
+#                 writer.writerow(("FN",lst_data[0] if lst_data else ''))
+#                 writer.writerow(("TEL;TYPE=CELL",lst_data[1] if lst_data else ''))
+#                 writer.writerow(("TEL;TYPE=WORK", lst_data[2] if lst_data else ''))
+#                 writer.writerow(("EMAIL;TYPE=WORK",lst_data[3] if lst_data else ''))
+#                 writer.writerow(
+#                     ("ORG;CHARSET=UTF-8", lst_data[5] if lst_data else ''))
+#                 writer.writerow(("TITLE",lst_data[6] if lst_data else ''))
+#                 writer.writerow(("END", "VCARD"))
+# writer.writerow([name.encode('utf-8') for name in fields])
 #
 #         for data in rows:
 #             row = []
@@ -63,14 +73,69 @@ class ExcelExportView(ExcelExport):
 #                         pass
 #                 if d is False: d = None
 #
-#                 # Spreadsheet apps tend to detect formulas on leading =, + and -
-#                 if type(d) is str and d.startswith(('=', '-', '+')):
-#                     d = "'" + d
-#
-#                 row.append(d)
-#             writer.writerow(row)
-#
-#         fp.seek(0)
-#         data = fp.read()
-#         fp.close()
-#         return data
+#                 Spreadsheet apps tend to detect formulas on leading =, + and -
+                # if type(d) is str and d.startswith(('=', '-', '+')):
+                #     d = "'" + d
+                #
+                # row.append(d)
+            # writer.writerow(row)
+        #
+        # fp.seek(0)
+        # data = fp.read()
+        # fp.close()
+        # return data
+class CSVExport(ExportFormat, http.Controller):
+
+    @http.route('/web/export/csv', type='http', auth="user")
+    @serialize_exception
+    def index(self, data, token):
+        return self.base(data, token)
+
+    @property
+    def content_type(self):
+        return 'text/csv;charset=utf8'
+
+    def filename(self, base):
+        return base + '.vcf'
+
+    def from_data(self, fields, rows):
+        fp = StringIO()
+        writer = csv.writer(fp, delimiter=":", quotechar='"')
+        for data in rows:
+            lst_data2 = []
+            lst_data2.append(data)
+            for lst_data in lst_data2:
+                writer.writerow(("BEGIN", "VCARD"))
+                writer.writerow(("VERSION", "3.0"))
+                writer.writerow(("N",lst_data[0] if lst_data else ''))
+                writer.writerow(("FN",lst_data[0] if lst_data else ''))
+                writer.writerow(("TEL;TYPE=CELL",lst_data[1] if lst_data else ''))
+                writer.writerow(("TEL;TYPE=WORK", lst_data[2] if lst_data else ''))
+                writer.writerow(("EMAIL;TYPE=WORK",lst_data[3] if lst_data else ''))
+                writer.writerow(
+                    ("ORG;CHARSET=UTF-8", lst_data[5] if lst_data else ''))
+                writer.writerow(("TITLE",lst_data[6] if lst_data else ''))
+                writer.writerow(("END", "VCARD"))
+writer.writerow([name.encode('utf-8') for name in fields])
+
+        for data in rows:
+            row = []
+            for d in data:
+                if isinstance(d, unicode):
+                    try:
+                        d = d.encode('utf-8')
+                    except UnicodeError:
+                        pass
+                if d is False: d = None
+
+                Spreadsheet apps tend to detect formulas on leading =, + and -
+                if type(d) is str and d.startswith(('=', '-', '+')):
+                    d = "'" + d
+
+                row.append(d)
+            writer.writerow(row)
+
+        fp.seek(0)
+        data = fp.read()
+        fp.close()
+        return data

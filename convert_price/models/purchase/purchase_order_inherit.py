7e4721764de6b9710f647de92e29ac2e40217c5d
foxbattrueplus <foxbat@trueplus.vn>
2017-05-18 13:39:07 +0700

    foxbat - module convert number to text (in sale order, purchase order and invoice), support english and vietnamese languages

---
+++ convert_price/models/purchase/purchase_order_inherit.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8-*-
+
+from odoo import api
+from odoo import fields
+from odoo import models
+from num2words import num2words
+
+
+class PurchaseOrder(models.Model):
+    _inherit = 'purchase.order'
+
+    amount_total_text = fields.Text(string='Total (In text)', store=False, readonly=True,
+                                    compute='_compute_amount_total_text')
+
+    @api.depends('amount_total')
+    def _compute_amount_total_text(self):
+        option = self._get_lang_config()
+        if option == 'eng':
+            for sale_order in self:
+                sale_order.amount_total_text ='In text: '+ num2words(sale_order.amount_total)
+        elif option == 'viet':
+            for sale_order in self:
+                sale_order.amount_total_text ='Bằng chữ: '+ self.env['convert.to.vn'].number_to_text(sale_order.amount_total)
+
+    @api.model
+    def _get_lang_config(self):
+        options = self.env['purchase.config.settings'].search([])
+        if len(options) == 0:
+            return 'eng'  # default
+        else:
+            return options[-1].language_option
+

8b2bbadbb4b688dcdd5675434c3f5a29658c57f7
Kriz <krizpham>
2017-05-18 10:07:47 +0700

    complete export

--- web_export_view/__manifest__.py
+++ web_export_view/__manifest__.py
@@ -1,10 +1,4 @@
 # -*- coding: utf-8 -*-
-# Copyright 2016 Henry Zhou (http://www.maxodoo.com)
-# Copyright 2016 Rodney (http://clearcorp.cr/)
-# Copyright 2012 Agile Business Group
-# Copyright 2012 Domsense srl (<http://www.domsense.com>)
-# Copyright 2012 Therp BV
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     'name': 'Web Export Current View',
@@ -20,6 +14,7 @@
     "data": [
         'views/web_export_view_view.xml',
         'views/inherit_employee_treeview.xml',
+        'views/inherit_contact_treeview.xml',
     ],
     'qweb': [
         "static/src/xml/web_export_view_template.xml",

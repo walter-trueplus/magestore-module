b8c5f3fd0efaab95493d298de1ef2c6c56be8ca0
walter <walter@trueplus.vn>
2017-05-19 14:25:18 +0700

    Merge branch 'dev-walter-local' into dev-local (reverted from commit e4c193ce76bb53eabefee1771502ba7c409afa22)

---
+++ blacklist_vendor/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'black list vendor',
+    'depends': ['base','purchase'],
+    'desciption': 'module for helping manager vendors in black list',
+    'data': [
+        'view/vendor_view.xml',
+        'view/setting.xml'
+    ],
+    'application': True,
+
+}

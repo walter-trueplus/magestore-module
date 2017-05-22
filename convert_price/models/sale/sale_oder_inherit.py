242497c114105610ef9a25a47d09c5cd07b44c4a
foxbattrueplus <foxbat@trueplus.vn>
2017-05-22 13:32:12 +0700

    foxbat: add currency unit after text

--- convert_price/models/sale/sale_oder_inherit.py
+++ convert_price/models/sale/sale_oder_inherit.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 
+from num2words import num2words
 from odoo import api
 from odoo import fields
 from odoo import models
-from num2words import num2words
 
 
 class SaleOrder(models.Model):
@@ -15,13 +15,19 @@ class SaleOrder(models.Model):
 
     @api.depends('amount_total')
     def _compute_amount_total_text(self):
-        option=self._get_lang_config()
-        if option=='eng':
+        option = self._get_lang_config()
+        for record in self:
+            print record.currency_id.id
+        if option == 'eng':
             for sale_order in self:
-                sale_order.amount_total_text ='In text: '+ num2words(sale_order.amount_total)
-        elif option=='viet':
+                sale_order.amount_total_text = 'In text: ' + num2words(sale_order.amount_total) + \
+                                               self._get_currency(sale_order.currency_id.id)
+
+        elif option == 'viet':
             for sale_order in self:
-                sale_order.amount_total_text ='Bằng chữ: '+ self.env['convert.to.vn'].number_to_text(sale_order.amount_total)
+                sale_order.amount_total_text = 'Bằng chữ: ' + \
+                                               self.env['convert.to.vn'].number_to_text(sale_order.amount_total) + \
+                                               self._get_currency(sale_order.currency_id.id)
 
     @api.model
     def _get_lang_config(self):
@@ -30,3 +36,7 @@ class SaleOrder(models.Model):
             return 'eng'  # default
         else:
             return options[-1].language_option
+
+    @api.model
+    def _get_currency(self, currency_id):
+        return " "+self.env['res.currency'].search([('id', '=', currency_id)]).name

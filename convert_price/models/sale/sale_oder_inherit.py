42d4a1ea53523ba289b57eaf0f329ac52a4ff845
foxbattrueplus <foxbat@trueplus.vn>
2017-05-22 16:40:01 +0700

    foxbat: debug conver_price

--- convert_price/models/sale/sale_oder_inherit.py
+++ convert_price/models/sale/sale_oder_inherit.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
 
+import sys
+
+reload(sys)
+sys.setdefaultencoding('utf8')
 from num2words import num2words
 from odoo import api
 from odoo import fields
@@ -15,28 +19,27 @@ class SaleOrder(models.Model):
 
     @api.depends('amount_total')
     def _compute_amount_total_text(self):
-        option = self._get_lang_config()
-        for record in self:
-            print record.currency_id.id
-        if option == 'eng':
-            for sale_order in self:
-                sale_order.amount_total_text = 'IN TEXT: ' + num2words(sale_order.amount_total).upper() + \
-                                               self._get_currency(sale_order.currency_id.id)
-
-        elif option == 'viet':
+        if self.env.user.lang == 'vi_VN':
             for sale_order in self:
                 sale_order.amount_total_text = 'BẰNG CHỮ: ' + \
                                                self.env['convert.to.vn'].number_to_text(sale_order.amount_total) + \
                                                self._get_currency(sale_order.currency_id.id)
-
-    @api.model
-    def _get_lang_config(self):
-        options = self.env['sale.config.settings'].search([])
-        if len(options) == 0:
-            return 'eng'  # default
         else:
-            return options[-1].language_option
+            for sale_order in self:
+                sale_order.amount_total_text = 'IN TEXT: ' + num2words(sale_order.amount_total).upper() + \
+                                               self._get_currency(sale_order.currency_id.id)
+
+    # @api.model
+    # def _get_lang_config(self):
+    #     if self.env.user.lang=='vi_VN':
+    #         return 'viet'
+    #     options = self.env['sale.config.settings'].search([])
+    #     if len(options) == 0:
+    #         return 'eng'  # default
+    #     else:
+    #         return options[-1].language_option
 
     @api.model
     def _get_currency(self, currency_id):
-        return " "+self.env['res.currency'].search([('id', '=', currency_id)]).name
+        name= " "+self.env['res.currency'].search([('id', '=', currency_id)]).name
+        return name

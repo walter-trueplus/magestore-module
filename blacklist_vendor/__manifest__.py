49fcf53d34cbe02bacd97c636519dbdea6d329a2
foxbattrueplus <foxbat@trueplus.vn>
2017-05-23 10:52:50 +0700

    foxbat: fixbug 2 module blacklist_vendor and convert_price

--- blacklist_vendor/__manifest__.py
+++ blacklist_vendor/__manifest__.py
@@ -1,12 +1,14 @@
 {
     'name': 'Black list vendor',
-    'depends': ['base','purchase','contacts'],
+    'depends': ['base', 'purchase', 'contacts'],
     'summary': """
         Notify while create purchase order in case the vendor in black list""",
 
     'description': """
         Notify while create purchase order in case the vendor in black list
     """,
+    'author': "Magestore.com",
+    'website': "Magestore.com",
     'data': [
         'view/vendor_view.xml',
         'view/setting.xml'
--- convert_price/models/sale/sale_oder_inherit.py
+++ convert_price/models/sale/sale_oder_inherit.py
@@ -1,9 +1,5 @@
 # -*- coding: utf-8 -*-
 
-import sys
-
-reload(sys)
-sys.setdefaultencoding('utf8')
 from num2words import num2words
 from odoo import api
 from odoo import fields
@@ -14,32 +10,25 @@ class SaleOrder(models.Model):
     _name = "sale.order"
     _inherit = 'sale.order'
 
-    amount_total_text = fields.Text(string='Total (In text)', store=False, readonly=True,
+    amount_total_text = fields.Text(string=u'Total (In text)', store=False, readonly=True,
                                     compute='_compute_amount_total_text')
 
-    @api.depends('amount_total')
+    @api.depends('amount_total','currency_id')
     def _compute_amount_total_text(self):
         if self.env.user.lang == 'vi_VN':
             for sale_order in self:
-                sale_order.amount_total_text = 'BẰNG CHỮ: ' + \
+                sale_order.amount_total_text = u'BẰNG CHỮ: ' + \
                                                self.env['convert.to.vn'].number_to_text(sale_order.amount_total) + \
                                                self._get_currency(sale_order.currency_id.id)
         else:
             for sale_order in self:
-                sale_order.amount_total_text = 'IN TEXT: ' + num2words(sale_order.amount_total).upper() + \
+                sale_order.amount_total_text = u'IN TEXT: ' + num2words(sale_order.amount_total).upper() + \
                                                self._get_currency(sale_order.currency_id.id)
-
-    # @api.model
-    # def _get_lang_config(self):
-    #     if self.env.user.lang=='vi_VN':
-    #         return 'viet'
-    #     options = self.env['sale.config.settings'].search([])
-    #     if len(options) == 0:
-    #         return 'eng'  # default
-    #     else:
-    #         return options[-1].language_option
 
     @api.model
     def _get_currency(self, currency_id):
-        name= " "+self.env['res.currency'].search([('id', '=', currency_id)]).name
-        return name
+        s=self.env['res.currency'].search([('id', '=', currency_id)]).name
+        if isinstance(s, basestring):
+            name= u" " + s
+            return name
+        else: return u' '

6e672410bd24e5241de5a873f299ae5e6b9b6df5
XMars <xtruongptit@gmail.com>
2017-05-25 15:44:50 +0700

    mars - add validate_email in menu General Settings() and method get validate_email attribute in controllers.py

--- website_my_profile/models/models.py
+++ website_my_profile/models/models.py
@@ -65,21 +65,6 @@ class IrMailServer(models.Model):
             self.create(vals)
 
 
-class BaseSettings(models.TransientModel):
-    _inherit = "base.config.settings"
-
-    @api.model
-    def set_domain_and_enable_reset_signup(self):
-        IrConfigParam = self.env['ir.config_parameter']
-        template_user_id = self.env['res.users'].search([('active', '=', False), ('login', '=', 'portaltemplate')]).id
-        if not template_user_id:
-            template_user_id = 1
-        IrConfigParam.set_param('mail.catchall.domain', 'trueplus.vn')
-        IrConfigParam.set_param('auth_signup.reset_password', 'True')
-        IrConfigParam.set_param('auth_signup.allow_uninvited', 'True')
-        IrConfigParam.set_param('auth_signup.template_user_id', template_user_id)
-
-
 class User(models.Model):
     _inherit = ['res.users']
 
@@ -87,11 +72,13 @@ class User(models.Model):
 
     @api.model
     def change_user_redirect_website_homepage(self):
-        action_id = self.env['ir.actions.actions'].search([('name', '=', 'Website Homepage')])[0].id
+        action_id = self.env['ir.actions.actions'].search(
+            [('name', '=', 'Website Homepage')])[0].id
         users_not_homepage = self.env['res.users'].search(['|',
                                                            ('active', '=', False),
                                                            '|',
-                                                           ('action_id', '!=', action_id),
+                                                           ('action_id',
+                                                            '!=', action_id),
                                                            ('action_id', '=', None)])
         for user in users_not_homepage:
             user.write({'action_id': action_id})
@@ -102,6 +89,56 @@ class User(models.Model):
         new_user = super(User, self).create(vals)
         if new_user:
             user = self.search([('id', '=', new_user.id)])[0]
-            action_id = self.env['ir.actions.actions'].search([('name', '=', 'Website Homepage')])[0].id
+            action_id = self.env['ir.actions.actions'].search(
+                [('name', '=', 'Website Homepage')])[0].id
             user.write({'action_id': action_id})
         return user
+
+
+class BaseSettings(models.TransientModel):
+    # all method of base.config.settings start with 'set_' will automatic run after
+    # user click Apply button on Settings/General Settings
+    # all method of base.config.settings start with 'get_' will automatic run after
+    # user click on menu General Settings
+    _inherit = 'base.config.settings'
+
+    validate_email_signup = fields.Boolean(
+        string='Enable validate email when user signup in Portal Website')
+
+    @api.multi
+    def set_auth_signup_template_user_id(self):
+        self.ensure_one()
+        IrConfigParam = self.env['ir.config_parameter']
+        # we store the repr of the values, since the value of the parameter is a
+        # required string
+        IrConfigParam.set_param('auth_signup.reset_password',
+                                repr(self.auth_signup_reset_password))
+        IrConfigParam.set_param('auth_signup.allow_uninvited',
+                                repr(self.auth_signup_uninvited))
+        IrConfigParam.set_param('auth_signup.template_user_id', repr(
+            self.auth_signup_template_user_id.id))
+        IrConfigParam.set_param('validate_email_signup',
+                                repr(self.validate_email_signup))
+
+    @api.model
+    def get_default_khong_get(self, fields):
+        # fet value of validate_email_signup and send it to base.config.settings object to show on view
+        value = self.env['ir.config_parameter'].search([('key', '=', 'validate_email_signup')], limit=1).value
+        return {'validate_email_signup': value}
+
+
+class MySettings(models.TransientModel):
+    _name = "my.config"
+
+    @api.model
+    def set_domain_and_enable_reset_signup(self):
+        IrConfigParam = self.env['ir.config_parameter']
+        template_user_id = self.env['res.users'].search(
+            [('active', '=', False), ('login', '=', 'portaltemplate')]).id
+        if not template_user_id:
+            template_user_id = 1
+        IrConfigParam.set_param('mail.catchall.domain', 'trueplus.vn')
+        IrConfigParam.set_param('auth_signup.reset_password', 'True')
+        IrConfigParam.set_param('auth_signup.allow_uninvited', 'True')
+        IrConfigParam.set_param(
+            'auth_signup.template_user_id', template_user_id)

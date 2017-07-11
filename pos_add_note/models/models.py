126b4d59cf4d71a052109aeb6301336f3e01217d
EvanTrueplus <evan@trueplus.vn>
2017-07-11 16:25:13 +0700

    Add note in POS Screen and note will be displayed in bill

---
+++ pos_add_note/models/models.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-#
+from odoo import models, fields, api, tools
+import logging
+import psycopg2
+
+_logger = logging.getLogger(__name__)
+
+
+class pos_order_inherit(models.Model):
+    _inherit = 'pos.order'
+
+    @api.model
+    def create_from_ui(self, orders):
+        submitted_references = [o['data']['name'] for o in orders]
+        pos_order = self.search([('pos_reference', 'in', submitted_references)])
+        existing_orders = pos_order.read(['pos_reference'])
+        existing_references = set([o['pos_reference'] for o in existing_orders])
+        orders_to_save = [o for o in orders if o['data']['name'] not in existing_references]
+        order_ids = []
+
+        for tmp_order in orders_to_save:
+            to_invoice = tmp_order['to_invoice']
+            order = tmp_order['data']
+            if to_invoice:
+                self._match_payment_to_invoice(order)
+            pos_order = self._process_order(order)
+            order_ids.append(pos_order.id)
+            new_order = self.search([('id', '=', pos_order.id)])
+            new_order.write({'note': tmp_order['data']['note']})
+
+            try:
+                pos_order.action_pos_order_paid()
+            except psycopg2.OperationalError:
+                # do not hide transactional errors, the order(s) won't be saved!
+                raise
+            except Exception as e:
+                _logger.error('Could not fully process the POS Order: %s', tools.ustr(e))
+
+            if to_invoice:
+                pos_order.action_pos_order_invoice()
+                pos_order.invoice_id.sudo().action_invoice_open()
+                pos_order.account_move = pos_order.invoice_id.move_id
+        return order_ids
